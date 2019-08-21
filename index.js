02c181899783a878388226e7a60e62616e5425df
daniel yomtov <yomtovdaniel1@gmail.com>
2019-08-21 12:32:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday} ${name}!`;
+}
+
+function holidayCountdown(holiday, number) {
+  return `It's ${number} days until ${holiday}!`
 }
